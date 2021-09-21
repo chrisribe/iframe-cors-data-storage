0c0ba28fb20ed0ea141dcd78f9130438a50e53d8
Ribe, Christian <Christian.Ribe@nuance.com>
2021-09-21 14:40:26 -0400

    ahh same domain !

--- code1.js
+++ code1.js
@@ -34,7 +34,7 @@
 	}
 
 	document.addEventListener("DOMContentLoaded", function(event) { 
-		loadScript("code2.js", 
+		loadScript("https://chrisribe.github.io/dud-page/code2.js", 
 			function(){
 				console.log('code 2 was init!');
 			}, 
